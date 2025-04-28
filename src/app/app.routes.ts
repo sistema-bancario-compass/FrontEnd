d71750b61596af305ecc56f1de3efaf4106b3707
Lucas Amorim Silva <lucas.amorim@compasso.com.br>
2025-04-28 20:40:02 -0300

    feat: new menu component and angular framework

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+
+export const routes: Routes = [{ path: '', component: MenuComponent }];
