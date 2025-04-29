24c1cf8960abb2dec284e10d0a3384d015a25c07
Lucas Amorim Silva <lucas.amorim@compasso.com.br>
2025-04-29 15:44:27 -0300

    fix: account page

--- src/app/pages/account-management/account-management.component.ts
+++ src/app/pages/account-management/account-management.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { InputPrimaryComponent } from '../../components/input-primary/input-primary.component';
-import { CommonModule } from '@angular/common'; // <-- Adiciona esta linha
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-account-management',
   standalone: true,
-  imports: [InputPrimaryComponent, CommonModule], // <-- Adiciona o CommonModule aqui
+  imports: [InputPrimaryComponent, CommonModule],
   templateUrl: './account-management.component.html',
   styleUrls: ['./account-management.component.scss'],
 })
@@ -14,5 +14,31 @@ export class AccountManagementComponent {
 
   selectTab(tab: string) {
     this.selectedTab = tab;
+  }
+
+  getActionButtonText(): string {
+    switch (this.selectedTab) {
+      case 'create':
+        return 'Create Account';
+      case 'deposit':
+        return 'Make Deposit';
+      case 'withdraw':
+        return 'Make Withdrawal';
+      default:
+        return 'Submit';
+    }
+  }
+
+  onSubmit() {
+    if (this.selectedTab === 'create') {
+      console.log('Creating account...');
+      // Aqui você chama a função de criar conta
+    } else if (this.selectedTab === 'deposit') {
+      console.log('Making deposit...');
+      // Aqui você chama a função de depósito
+    } else if (this.selectedTab === 'withdraw') {
+      console.log('Making withdrawal...');
+      // Aqui você chama a função de saque
+    }
   }
 }
