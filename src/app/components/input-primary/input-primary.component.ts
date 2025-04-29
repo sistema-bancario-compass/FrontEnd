4e29c1c1cd24d8570b19cef40edd1be974ec6a6d
Lucas Amorim Silva <lucas.amorim@compasso.com.br>
2025-04-29 12:37:07 -0300

    fix:input

--- src/app/components/input-primary/input-primary.component.ts
+++ src/app/components/input-primary/input-primary.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-input-select',
+  selector: 'app-input-primary',
   imports: [],
-  templateUrl: './input-select.component.html',
-  styleUrl: './input-select.component.scss',
+  templateUrl: './input-primary.component.html',
+  styleUrl: './input-primary.component.scss',
 })
 export class InputPrimaryComponent {
   @Input() label: string = 'Input Label';
   @Input() name: string = '';
-  @Input() type: string = 'text';
+  @Input() type: 'text' | 'email' | 'password' | 'date' = 'text';
   @Input() placeholder: string = '';
   @Input() value: string = '';
 }
