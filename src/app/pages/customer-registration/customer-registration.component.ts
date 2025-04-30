e419b858df8206e6d30b051c0c0cfb1b4c34e059
Lucas Amorim Silva <lucas.amorim@compasso.com.br>
2025-04-29 21:45:52 -0300

    fix:costumer

--- src/app/pages/customer-registration/customer-registration.component.ts
+++ src/app/pages/customer-registration/customer-registration.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ButtonComponent } from '../../components/ui/button/button.component';
 
 interface Customer {
   id: string;
